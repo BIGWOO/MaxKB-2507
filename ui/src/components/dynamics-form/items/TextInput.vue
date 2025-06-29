836f871431379fc82a393516cb02f9134ec6968c
CHUN YEN WU <bigwoo@gmail.com>
2025-06-30 03:14:26 +0800

    更新 TextInput.vue 組件，將 el-input 的類型更改為 textarea，並新增自動調整大小的屬性，以提升使用者輸入體驗。

--- ui/src/components/dynamics-form/items/TextInput.vue
+++ ui/src/components/dynamics-form/items/TextInput.vue
@@ -1,5 +1,9 @@
 <template>
-  <el-input v-bind="$attrs" />
+  <el-input 
+    v-bind="$attrs"
+    type="textarea" 
+    :autosize="{ minRows: 1, maxRows: 4 }"
+  />
 </template>
 <script setup lang="ts"></script>
 <style lang="scss"></style>
