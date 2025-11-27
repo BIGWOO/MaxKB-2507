715adc13c51782c4d2bea36653aaef4d2b18e30f
liqiang-fit2cloud <liqiang@fit2cloud.com>
2025-11-27 15:41:51 +0800

    deps: upgrade redis-server.

--- apps/common/utils/tool_code.py
+++ apps/common/utils/tool_code.py
@@ -254,7 +254,7 @@ exec({dedent(code)!a})
             if proc.poll() is None: #如果仍未终止，强制终止
                 os.killpg(pgid, signal.SIGKILL)
             proc.wait()
-            raise Exception(_("Sandbox process execution timeout, consider increasing MAXKB_SANDBOX_PYTHON_PROCESS_TIMEOUT_SECONDS."))
+            raise Exception(_("Process execution timed out.")) # 执行超时，到了MAXKB_SANDBOX_PYTHON_PROCESS_TIMEOUT_SECONDS限制
 
     def validate_mcp_transport(self, code_str):
         servers = json.loads(code_str)
